aae581eaae760cf7c7fbac812297cb62e01e4350
Khazixi <khazixfangirl@gmail.com>
2023-05-11 22:45:25 -0400

    Added Prisma to patch my lack of SQL Knowledge

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import PrismaClient from '@prisma/client'
 
 const app = express();
+const prisma = new PrismaClient()
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + Vue + TypeScript!");
