05b2a980f156d4a78250264dee168964c3c355e2
Khazixi <khazixfangirl@gmail.com>
2023-08-28 16:38:45 -0400

    Hoping this fixes vercel bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     strict: true,
   },
   nitro: {
-    preset: 'vercel',
+		moduleSideEffects: ["lucia/polyfill/node"],
   },
   pinia: {
     autoImports: ["defineStore", ["defineStore", "definePiniaStore"]],
