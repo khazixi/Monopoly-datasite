d5a6ef12c028a896d6afc8e3a08bfa6b820b457d
Khazixi <khazixfangirl@gmail.com>
2023-08-28 11:54:26 -0400

    Added Pollyfill to lucia

--- server/util/lucia.ts
+++ server/util/lucia.ts
@@ -2,6 +2,7 @@ import { prisma } from "@lucia-auth/adapter-prisma";
 import { lucia } from "lucia";
 import { h3 } from "lucia/middleware";
 import { prisma as db } from "./db";
+import "lucia/polyfill/node"
 
 export const auth = lucia({
   adapter: prisma(db),
