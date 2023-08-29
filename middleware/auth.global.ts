7d5a12e7d72dee29eed90ae95a8ea86b4765e54e
Khazixi <khazixfangirl@gmail.com>
2023-08-29 11:09:08 -0400

    The rock almost crushes him

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,6 +3,7 @@ import { useUser } from "../composables/auth";
 export default defineNuxtRouteMiddleware(async () => {
   const user = useUser();
   const { data, error } = await useFetch('/api/user');
-  if (error.value) throw createError("Failed to Fetch Data");
+  // WARNING: Uncomment Later, deleting to debug Vercel
+  // if (error.value) throw createError("Failed to Fetch Data");
   user.value = data.value?.user ?? null;
 });
